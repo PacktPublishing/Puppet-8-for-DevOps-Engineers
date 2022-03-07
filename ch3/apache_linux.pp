828b91a0d05866ec58e1c639b59b1c1406d77b17
David Sandilands <david@puppet.com>
2022-03-07 00:11:42 +0000

    Adding apache and grafana examples

---
+++ ch3/apache_linux.pp
@@ -0,0 +1,22 @@
+package { 'httpd':}
+
+service { 'httpd':
+  ensure       => 'running',
+  enable       => 'true',
+  require      => package['httpd']
+}
+
+file { '/var/www/html/index.html ':
+  ensure   => 'file',
+  content  => '<html>\n<head>\n</head>\n<body>\n  <h1>Hello World<h1>\n</body>\n</html>',
+  notify   => Service['httpd'],
+  require  => Package['httpd']
+}
+
+file { '/etc/httpd/conf/httpd.conf ':
+  ensure       => 'file',
+  source       => '',
+  notify       => Service['httpd'],
+  require      => Package['httpd'],
+  validate_cmd => '/usr/sbin/apache2 -t -f %'
+}
---
+++ ch3/grafana_windows.pp
@@ -0,0 +1,15 @@
+package { 'grafana':
+  source => 'https://dl.grafana.com/oss/release/grafana-8.4.3.windows-amd64.msi'
+}
+service { 'grafana':
+  ensure       => 'running',
+  enable       => 'true',
+  logonaccount => 'LocalSystem',
+  require      => package['grafana']
+}
+file { 'C:\Program Files\GrafanaLabs\grafana\conf\grafana.ini':
+  ensure   => 'file',
+  content  => '[server]\nprotocol = HTTP\nhttp_port = 8080',
+  notify   => Service['grafana'],
+  require  => Package['grafana']
+}
