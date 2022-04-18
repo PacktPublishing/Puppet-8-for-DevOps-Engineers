fbf3164d15f394f0c0593b68d1e9544648b50f7d
David Sandilands <david@puppet.com>
2022-04-19 00:09:40 +0100

    first draft and corrections

--- ch4/all_grafana_data_types.pp
+++ ch4/all_grafana_data_types.pp
@@ -1,12 +1,23 @@
-Class {'all_grafana':
-  port            => 8080,
-  service_enabled => true
+Class {'all_grafana_data_types':
+  port             => 8080,
+  service_enabled  => true,
+  file_options     => { name  => '/etc/grafana/grafana.ini',
+                        mode  => 0550,
+                        user  => 'grafana',
+                        group => 'root'
+                      },
+  grafana_password =>  'secretpassword'
 }
 
-class all_grafana (
+class all_grafana_data_types (
   Integer $port,
   String $download_source = 'https://dl.grafana.com/enterprise/release/grafana-enterprise-8.4.3-1.x86_64.rpm',
-  Boolean $service_enabled
+  Boolean $service_enabled,
+  Struct[{name           => String,
+          Optional[mode] => Integer[4],
+          Optional[user] => String,
+          Optional[group] => String }] $file_options,
+  Sensitive $grafana_password
 )
 {
 case $facts['os']['family'] {
@@ -14,15 +25,9 @@ case $facts['os']['family'] {
     $service options = {
       'logonaccount' => 'LocalSystem'
     }
-    $file_options = {
-      'name' => 'C:\Program Files\GrafanaLabs\grafana\conf\grafana.ini'
-    }
   }
   default:  {
     $service options = ''
-    $file_options = {
-      'name' => '/etc/grafana/grafana.ini '
-    }
   }
 }
 
