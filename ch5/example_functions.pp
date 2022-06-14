63bc6f2746f945f411eba14fa0b19e43b0772f11
davidsandilands <david@puppet.com>
2022-06-14 11:25:44 +0100

    Draft of functions class

---
+++ ch5/example_functions.pp
@@ -0,0 +1,26 @@
+Class {'example_functions':
+  user_prefix => 'user',
+  user_number => 5
+}
+
+
+class example_functions (
+  String $user_prefix,
+  Integer $user_number
+)
+{
+  $lowercase_prefix = $user_prefix.lowercase()
+  $users_to_create = range("${lowercase_prefix}0","${lowercase_prefix}${user_number}")
+  user { "${users_to_create}": }
+
+  warning("getvar('os.windows.product_name','linux') ")
+
+  if $facts['os.family'] == 'windows' {
+    $audit_files = split($example_split, /;/)
+  } else {
+    $audit_files = split($example_split, /:/)
+  }
+  files { "$audit_files" :
+    audit => true
+  }
+}
