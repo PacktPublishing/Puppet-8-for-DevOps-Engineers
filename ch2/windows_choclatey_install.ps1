9f759ac2f0f07700bcd7a42ea65cff0ab600bdf0
David Sandilands <david@puppet.com>
2022-06-23 23:32:15 +0100

    Update windows_choclatey_install.ps1
    Removing versions and adding missing confirmations

--- ch2/windows_choclatey_install.ps1
+++ ch2/windows_choclatey_install.ps1
@@ -1,25 +1,25 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) 
  
-choco install pdk --version 2.3.0.0 -y 
+choco install pdk -y 
 
-choco install puppet-agent --version 7.13.1 -y 
+choco install puppet-agent -y 
 
-choco install vscode-puppet --version 1.4.0 -y 
+choco install vscode-puppet -y 
 
-choco install puppet-bolt --version 3.21.0 -y 
+choco install puppet-bolt -y 
 
-choco install vscode --version 1.63.2 -y 
+choco install vscode2 -y 
 
-choco install git --version 2.34.1 -y 
+choco install git -y 
 
-choco install pe-client-tools --version 21.4.1 -y 
+choco install pe-client-tools -y 
 
-choco install gh --version 2.4.0 -y 
+choco install gh -y 
 
-choco install azure-cli 
+choco install azure-cli -y
 
-choco install shellcheck 
+choco install shellcheck -y
 
-Install-Module PuppetBolt 
+Install-Module PuppetBolt -y
 
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0 
